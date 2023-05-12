718861def79d4e569a7b16bed7d37e024a9eef5c
Swoorup Joshi <swooruptrader@gmail.com>
2023-05-13 01:31:25 +1000

    Upload to npm.io instead

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,13 @@
-import { defineConfig } from 'vite';
-import path from 'path';
+import path from 'path'
+import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    base: "./",
-    build: {
-        lib: {
-            entry: path.resolve(__dirname, 'src/index.ts'),
-            name: 'the-sorted-array',
-            fileName: (format) => `index.${format}.js`
-        },
-        rollupOptions: {
-            // Some libraries do not work well with Rollup and should be listed here.
-            // Examples are 'gl' and 'fs'.
-            external: [],
-        },
-        sourcemap: true,
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "lib/index.ts"),
+      name: 'SortedArray',
+      fileName: (format) => `the-sorted-array.${format}.js`,
     },
-    define: {
-        global: {},
-    },
-    optimizeDeps: {
-        disabled: true,
-    },
-    plugins: [],
-});
+    sourcemap: true,
+  },
+})
